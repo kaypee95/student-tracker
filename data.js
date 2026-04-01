9dd0a752d36164e18fa67523185293c909704f53
kaypee95 <kpgrafix95@gmail.com>
2026-04-01 13:33:02 +0000

    Add initial student data

---
+++ data.js
@@ -0,0 +1,16 @@
+// Initial student data
+const students = [
+  { id: 1,  name: "Fatou Kujabi",    age: 20, grade: "A", attendance: 95 },
+  { id: 2,  name: "Lamin Touray",    age: 22, grade: "B", attendance: 88 },
+  { id: 3,  name: "Aminata Jallow",  age: 21, grade: "A", attendance: 92 },
+  { id: 4,  name: "Ousman Ceesay",   age: 23, grade: "C", attendance: 75 },
+  { id: 5,  name: "Binta Camara",    age: 20, grade: "B", attendance: 85 },
+  { id: 6,  name: "Modou Jobe",      age: 24, grade: "A", attendance: 97 },
+  { id: 7,  name: "Isatou Baldeh",   age: 21, grade: "C", attendance: 70 },
+  { id: 8,  name: "Ebrima Drammeh",  age: 22, grade: "B", attendance: 80 },
+  { id: 9,  name: "Mariama Sanneh",  age: 20, grade: "A", attendance: 90 },
+  { id: 10, name: "Yankuba Fatty",   age: 23, grade: "C", attendance: 72 }
+];
+
+// Export so other files can use it
+module.exports = { students };
